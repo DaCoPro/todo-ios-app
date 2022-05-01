8a005a81cbe06618eee9e86559f2c4d395d8ebe2
DaCoPro <comerbdavid@gmail.com>
2022-04-30 23:28:40 -0700

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by David Comer on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
